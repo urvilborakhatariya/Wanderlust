8cb12373018a0259bbcc7243b5a1af80f3aad35f
Urvil Borakhatariya <urvilborakhatariya@gmail.com>
2025-10-20 00:05:49 +0530

    Add project files

---
+++ controllers/user.js
@@ -0,0 +1,43 @@
+const User = require("../models/user.js");
+
+module.exports.renderSignupForm = (req, res) => {
+    res.render("user/signup.ejs");
+};
+
+module.exports.signup = async (req, res) => {
+    try {
+        let { username, email, password } = req.body;
+        const newUser = new User({ username, email });
+        const registeredUser = await User.register(newUser, password);
+        req.login(registeredUser, (err) => {
+            if (err) {
+                return next(err);
+            }
+            req.flash("success", "welcome to wanderlust");
+            res.redirect("/listings");
+        });
+    } catch (err) {
+        req.flash("error", err.message);
+        res.redirect("/signup");
+    }
+};
+
+module.exports.renderLoginForm = (req, res) => {
+    res.render("user/login.ejs");
+};
+
+module.exports.login = async (req, res) => {
+    req.flash("success", "You have logged in");
+    let redirectUrl = res.locals.redirectUrl || "/listings";
+    res.redirect(redirectUrl);
+};
+
+module.exports.logout = (req, res, next) => {
+    req.logout((err) => {
+        if (err) {
+            return next(err);
+        }
+        req.flash("success", "You are logged out");
+        res.redirect("/listings");
+    });
+};
